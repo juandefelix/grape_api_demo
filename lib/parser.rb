e237534baf7a018267bf3e2131c20113700ecbf7
juandefelix <juandefelix@yahoo.es>
2016-03-30 19:08:47 -0500

    adds tests for format and renames methods

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,16 +1,23 @@
+require 'pry-byebug'
 class StringParser
   FORMAT_REGEX =  Regexp.union([
     /\s?(\w+)\s\|\s(\w+)\s\|\s(\w+)\s\|\s(\w+)\s\|\s([\d\/-]+)\s?/,
     /\s?(\w+),\s?(\w+),\s?(\w+),\s?(\w+),\s?([\d\/-]+)\s?/
   ])
 
-  attr_reader :raw_input
+  attr_reader :user_input
 
-  def initialize(raw_input)
-    @raw_input = raw_input
+  def initialize(user_input)
+    @user_input = user_input
   end
 
-  def parse
-    [$1, $2, $3, $4, $5].join(', ') if raw_input =~ FORMAT_REGEX
+  def serialize_input
+    [$1, $2, $3, $4, $5].join(',') if user_input =~ FORMAT_REGEX
+  end
+
+  private
+
+  def valid_string?
+    
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,25 +1,30 @@
 require 'spec_helper'
 
 RSpec.describe StringParser do
-  describe '#parse' do
+  describe '#serialize_input' do
     before(:each) do
       @parser = StringParser.new('Ortiz | Juan | Male | Blue | 07-26-1976')
     end
 
-    it 'receives a string as parameter' do 
-      expect(@parser).to receive :parse
-      @parser.parse
+    it 'it is an public instance method' do 
+      expect(@parser).to receive(:serialize_input)
+      @parser.serialize_input
     end
 
     it 'returns a hash with parsed values' do
-      expected_results  = 'Ortiz, Juan, Male, Blue, 07-26-1976'
-      expect(@parser.parse).to eq expected_results 
+      expected_results  = 'Ortiz,Juan,Male,Blue,07-26-1976'
+      expect(@parser.serialize_input).to eq expected_results 
     end
 
     it 'accepts more than one format' do
       parser = StringParser.new('Ortiz, Juan, Male, Blue, 07-26-1976')
-      expected_results  = 'Ortiz, Juan, Male, Blue, 07-26-1976'
-      expect(@parser.parse).to eq expected_results 
+      expected_results  = 'Ortiz,Juan,Male,Blue,07-26-1976'
+      expect(@parser.serialize_input).to eq expected_results 
+    end
+
+    it 'user_input is nil when format is not correct' do
+      wrong_format_string = 'Ortiz || Juan | Male | Blue | 07-26-1976'
+      expect(StringParser.new(wrong_format_string).serialize_input).to eq nil
     end
   end
 end
