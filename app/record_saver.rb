18637337dd55b398ff70db72ef04af9f98e740e2
juandefelix <juandefelix@yahoo.es>
2016-03-31 17:52:10 -0500

    add record saver class and tests

---
+++ app/record_saver.rb
@@ -0,0 +1,20 @@
+class RecordSaver
+  def initialize(input)
+    @input = input
+  end
+
+  def save
+    status = saved? ? 'ok' : 'failed' 
+    { status: status }
+  end
+
+  private
+
+  def saved?
+    record && List.write(record) 
+  end
+
+  def record
+    StringParser.new(@input).serialize_input
+  end
+end
---
+++ spec/record_saver_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+RSpec.describe RecordSaver do  
+  describe '#save' do
+    it 'return status ok with forperly formatted string' do
+      saver = RecordSaver.new('Byrne, Joan, Female, Green, 3-12-1987')
+      expect(saver.save).to eq({ status: 'ok' })
+    end
+
+    it 'return status ok with forperly formatted string' do
+      saver = RecordSaver.new('Byrne, ||Joan, Female, Green, 3-12-1987')
+      expect(saver.save).to eq({ status: 'failed' })
+    end
+  end
+end
