2633b5fdd5fdcf076a490f92da761fa3a14e24b2
juandefelix <juandefelix@yahoo.es>
2016-03-31 23:54:26 -0500

    replacing contents of file

--- spec/record_saver_spec.rb
+++ spec/record_saver_spec.rb
@@ -2,9 +2,18 @@ require 'spec_helper'
 
 RSpec.describe RecordSaver do 
   describe '#save' do
+    after(:each) do
+      doc = <<-STR
+corey, juan, male,red, 07-26-1976
+abbott, glenn, male,blue, 06-22-1999
+murphy, janet, female,green, 03-13-1999
+ortiz, eve, female,red, 05-07-1969
+      STR
+      File.open('./spec/support/records.txt', 'w') { |f| f.write(doc)}
+    end
+    
     it 'return status ok with forperly formatted string' do
       file = double()
-      allow(File).to receive(:open).with('./app/records.txt', 'a').and_return(file)
       saver = RecordSaver.new('Byrne, Joan, Female, Green, 3-12-1987')
       expect(saver.save).to eq({ status: 'ok' })
     end
