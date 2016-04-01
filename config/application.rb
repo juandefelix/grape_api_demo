aa98a2d247e438e4935bde1d6af4398d09a7dae3
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2016-04-01 10:46:42 -0500

    reading file location from global variable

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,11 @@ require 'rubygems'
 require 'bundler/setup'
 
 Bundler.require :default
+env_file = File.expand_path('../local_env.yml', __FILE__)
+yaml = YAML.load(File.open(env_file))
+$FILE_LOCATION = yaml[ENV['RACK_ENV']]['default_file_location']
 
 Dir[File.expand_path('../../app/*.rb', __FILE__)].each do |f|
   require f
 end
+
